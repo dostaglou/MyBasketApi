1d383487023445bf550e1776647d2c204f46ccd0
Dillon Wyatt <dostaglou@gmail.com>
2022-11-12 21:49:08 +0900

    annotation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string           not null
+#  password   :string           not null
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 class User < ApplicationRecord
+  
 end
