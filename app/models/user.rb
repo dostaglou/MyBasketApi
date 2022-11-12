f9aa2aba114eba66db6d232e3f54a26a50f4d1fb
Dillon Wyatt <dostaglou@gmail.com>
2022-11-12 22:11:53 +0900

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,16 @@
 #  index_users_on_email  (email) UNIQUE
 #
 class User < ApplicationRecord
-  
+  encrypts :password # non-determanistic encryption into the DB. 
+  validates :email, :password, :username, presence: true
+  validates :password, length: { in: 6..20 }
+  validates :username, length: { in: 3..40 }
+  validates :email, uniqueness: true, if: :email_validator
+
+  private 
+    def email_validator
+      unless self.email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+        self.errors.add(:email, :not_a_valid_email)
+      end
+    end
 end
