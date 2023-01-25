36c3e93bafd51af31b9c46c8879f6ccc5bfee932
Dillon Wyatt <dostaglou@gmail.com>
2023-01-25 22:29:04 +0900

    statuses

--- app/graphql/resolvers/shopping_list_item_search.rb
+++ app/graphql/resolvers/shopping_list_item_search.rb
@@ -1,9 +1,13 @@
 class Resolvers::ShoppingListItemSearch < Resolvers::Base
   argument :shopping_list_item_id, ID, required: false
   argument :shopping_list_id, ID, required: false
+  argument :statuses,
+           [Types::ShoppingListItems::ShoppingListItemStatusEnum],
+           required: false,
+           default_value: ShoppingListItem.statuses.keys
 
-  def resolve(shopping_list_id: nil, shopping_list_item_id: nil)
-    scope = current_user.shopping_list_items
+  def resolve(statuses:, shopping_list_id: nil, shopping_list_item_id: nil)
+    scope = current_user.shopping_list_items.where(status: statuses)
 
     return [scope.find(shopping_list_item_id)] if shopping_list_item_id
 
