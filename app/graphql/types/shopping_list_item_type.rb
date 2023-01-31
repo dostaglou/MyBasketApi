e465b52dd808fdb345b00d32fe5aa2531804fd07
Dillon Wyatt <dostaglou@gmail.com>
2023-01-31 23:43:22 +0900

    edit of shopping list item type

--- app/graphql/types/shopping_list_item_type.rb
+++ app/graphql/types/shopping_list_item_type.rb
@@ -4,4 +4,6 @@ class Types::ShoppingListItemType < Types::BaseObject
   field :catalogue_item_id, ID, null: false
   field :status, String, null: false
   field :quantity, Integer, null: false
+  field :name, String, null: false
+  field :description, String, null: false
 end
